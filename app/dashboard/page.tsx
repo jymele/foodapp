7e8d286cb58b391a51e6729e9fa46ff829b9514f
Joel Ymele <joel.ymele@gmail.com>
2025-05-14 00:31:28 -0400

    Add Card component and update DashboardPage layout for improved UI

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default async function DashboardPage() {
   const session = await auth();
@@ -8,6 +9,10 @@ export default async function DashboardPage() {
   }
 
   return (
-    <div className="mt-14 container mx-auto">Hello {session.user?.name}</div>
+    <div className="mt-14 container mx-auto">
+      <Card>
+        <CardContent>Hello {session.user?.name}</CardContent>
+      </Card>
+    </div>
   );
 }
