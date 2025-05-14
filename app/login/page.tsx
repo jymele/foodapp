0432eac4352463c838bfc17ae0f1488119408b0d
Joel Ymele <joel.ymele@gmail.com>
2025-05-13 23:44:19 -0400

    Add Radix UI button component and login page

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function LoginPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Button>Login with Google</Button>
+    </div>
+  );
+}
