4ccf912df191cc77c82d693323bb2da0bb9083ae
Joel Ymele <jymele@yahoo.fr>
2025-05-14 12:17:35 -0400

    Update NewRoom page layout and add paragraph styling for improved readability

--- app/newRoom/page.tsx
+++ app/newRoom/page.tsx
@@ -1,7 +1,7 @@
 export default function NewRoom() {
   return (
-    <div>
-      <h1>New Room</h1>
+    <div className="container mx-auto p-2 mt-14">
+      <h1>Create a Room</h1>
       <p>This is the new room page.</p>
     </div>
   );
