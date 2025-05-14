27a9fba720c626faf0af4a4d0d2fb70a7f944406
Joel Ymele <joel.ymele@gmail.com>
2025-05-14 00:21:38 -0400

    Implement session-based redirection in DashboardPage: authenticate user and redirect to login if not authenticated

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,13 @@
-export default function DashboardPage() {
-  return <div className="mt-14 container mx-auto">Dashboard Page</div>;
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const session = await auth();
+  if (!session) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="mt-14 container mx-auto">Hello {session.user?.name}</div>
+  );
 }
