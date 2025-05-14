796868bbc52ca8762d204583d92dac39c5a77001
Joel Ymele <jymele@yahoo.fr>
2025-05-14 12:13:44 -0400

    Created the prisma client

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import { Card, CardContent } from "@/components/ui/card";
+import { PrismaClient } from "@/generated/prisma";
 
 export default async function DashboardPage() {
   const session = await auth();
@@ -8,10 +9,24 @@ export default async function DashboardPage() {
     redirect("/login");
   }
 
+  const prisma = new PrismaClient();
+  const rooms = await prisma.userToRoom.findMany({
+    where: { userEmail: session.user!.email as string },
+  });
+
+  if (rooms.length === 0) {
+    redirect("/newRoom");
+  }
+
+  console.log("User's rooms:", rooms);
+
   return (
     <div className="mt-14 container mx-auto p-2">
       <Card>
-        <CardContent>Hello {session.user?.name}</CardContent>
+        <CardContent>
+          <p>Hello {session.user?.name}</p>
+          <p>{session.user?.email}</p>
+        </CardContent>
       </Card>
     </div>
   );
---
+++ app/newRoom/page.tsx
@@ -0,0 +1,8 @@
+export default function NewRoom() {
+  return (
+    <div>
+      <h1>New Room</h1>
+      <p>This is the new room page.</p>
+    </div>
+  );
+}
