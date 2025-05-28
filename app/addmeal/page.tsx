8172f8459f202510b45794227aafff15f579fb75
TFO\jymele <jymele@tfo.org>
2025-05-28 18:19:59 -0400

    Refactor AddMealPage: update input fields for date and description, improving form structure

--- app/addmeal/page.tsx
+++ app/addmeal/page.tsx
@@ -9,10 +9,10 @@ export default function AddMealPage() {
       <Form action={submitMeal} className="flex flex-col gap-4 p-4">
         <h1 className="text-lg font-semibold mb-4">Add Meal</h1>
         <input name="meal-name" placeholder="Meal Name" className="input" />
-        <input
+        <input type="date" name="date" className="input" />
+        <textarea
           name="description"
-          type="text"
-          placeholder="Meal Name"
+          placeholder="Description"
           className="input"
         />
         <SubmitButton>
