6a97e220c8903b6fa02d664db4942d446517f920
TFO\jymele <jymele@tfo.org>
2025-06-02 21:32:32 -0400

    Refactor DashboardClient layout and enhance toggle button styles for improved usability

--- app/dashboard/client.tsx
+++ app/dashboard/client.tsx
@@ -14,23 +14,25 @@ export default function DashboardClient({ todaysMeals, weeksMeals }: Props) {
 
   return (
     <div className="page">
-      <div className="flex justify-center items-center gap-4">
-        <button
-          className="toggle-button disabled:text-slate-400 cursor-pointer"
-          onClick={() => setToggleDay(true)}
-          aria-disabled={toggleDay}
-          disabled={toggleDay}
-        >
-          Today
-        </button>
-        <button
-          className="toggle-button"
-          onClick={() => setToggleDay(false)}
-          aria-disabled={!toggleDay}
-          disabled={!toggleDay}
-        >
-          This Week
-        </button>
+      <div className="p-2">
+        <div className="mx-auto rounded-full bg-slate-200  p-1 w-fit flex gap-1">
+          <button
+            className="toggle-button disabled:text-slate-400 cursor-pointer"
+            onClick={() => setToggleDay(true)}
+            aria-disabled={toggleDay}
+            disabled={toggleDay}
+          >
+            Today
+          </button>
+          <button
+            className="toggle-button"
+            onClick={() => setToggleDay(false)}
+            aria-disabled={!toggleDay}
+            disabled={!toggleDay}
+          >
+            This Week
+          </button>
+        </div>
       </div>
       <AnimatePresence>
         {toggleDay && (
@@ -55,14 +57,6 @@ export default function DashboardClient({ todaysMeals, weeksMeals }: Props) {
             <MealList meals={weeksMeals} />
           </motion.div>
         )}
-        {/* <motion.div>
-          <h1 className="text-lg font-semibold mb-4">Today's Meals</h1>
-          <MealList meals={todaysMeals} />
-        </motion.div>
-        <motion.div>
-          <h1 className="text-lg font-semibold mb-4">This Week's Meals</h1>
-          <MealList meals={weeksMeals} />
-        </motion.div> */}
       </AnimatePresence>
     </div>
   );
