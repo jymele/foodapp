f1c006463c15ee7b509bb0c893ca399a38a6f830
Joel Ymele <jymele@yahoo.fr>
2025-06-02 23:31:50 -0400

    Add LoaderCircle and FaGoogle icons to SignIn button; update package.json and package-lock.json to include react-icons

--- custom/signin.tsx
+++ custom/signin.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState } from "react";
 import { signIn } from "next-auth/react";
+import { LoaderCircle } from "lucide-react";
+import { FaGoogle } from "react-icons/fa";
 
 export default function SignIn() {
   const [loading, setLoading] = useState(false);
@@ -8,13 +10,15 @@ export default function SignIn() {
   return (
     <button
       aria-disabled={loading}
-      className="btn bg-white outline-slate-500/0 focus:outline-slate-400"
+      disabled={loading}
+      className="btn bg-white outline-slate-500/0 focus:outline-slate-400 flex gap-4 items-center justify-center text-slate-950 hover:bg-slate-100 active:scale-95 transition duration-200 focus:ring-2 focus:ring-slate-300 focus:ring-offset-2 rounded-lg shadow-sm w-full max-w-xs h-12"
       onClick={() => {
         setLoading(true);
         signIn("google", { callbackUrl: "/dashboard" });
       }}
     >
-      {loading ? "Loading..." : "Login with Google"}
+      {loading ? <LoaderCircle className="animate-spin" /> : <FaGoogle />}Login
+      with Google
     </button>
   );
 }
