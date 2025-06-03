7d68dd911e138637154d9de5cb78083ad2ab75f4
Joel Ymele <jymele@yahoo.fr>
2025-06-02 23:35:04 -0400

    Add disabled styles to SignIn button for better user feedback during loading

--- custom/signin.tsx
+++ custom/signin.tsx
@@ -11,7 +11,7 @@ export default function SignIn() {
     <button
       aria-disabled={loading}
       disabled={loading}
-      className="btn bg-white outline-slate-500/0 focus:outline-slate-400 flex gap-4 items-center justify-center text-slate-950 hover:bg-slate-100 active:scale-95 transition duration-200 focus:ring-2 focus:ring-slate-300 focus:ring-offset-2 rounded-lg shadow-sm w-full max-w-xs h-12"
+      className="btn bg-white outline-slate-500/0 focus:outline-slate-400 flex gap-4 items-center justify-center text-slate-950 hover:bg-slate-100 active:scale-95 transition duration-200 focus:ring-2 focus:ring-slate-300 focus:ring-offset-2 rounded-lg shadow-sm w-full max-w-xs h-12 disabled:cursor-not-allowed disabled:opacity-50"
       onClick={() => {
         setLoading(true);
         signIn("google", { callbackUrl: "/dashboard" });
