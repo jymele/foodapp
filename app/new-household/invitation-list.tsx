48c1f681686917f9913cf7b0818f635275452d1f
TFO\jymele <jymele@tfo.org>
2025-06-05 22:06:26 -0400

    Enhance InvitationList component to display invitations with motion effects

--- app/new-household/invitation-list.tsx
+++ app/new-household/invitation-list.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import { Invitation } from "@/generated/prisma";
+import { motion } from "motion/react";
 
 type Props = {
   invitations: Invitation[];
 };
 
 export default function InvitationList({ invitations }: Props) {
-  return <div>Invitations</div>;
+  return (
+    <div>
+      <h2>Invitations</h2>
+      <motion.div>
+        {invitations.map((invitation) => (
+          <div key={invitation.id}>{invitation.inviter_email}</div>
+        ))}
+      </motion.div>
+    </div>
+  );
 }
