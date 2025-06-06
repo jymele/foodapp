1bdc49319a63cf05de9aac9d7fe9189e40846a11
Joel Ymele <jymele@yahoo.fr>
2025-06-06 10:02:05 -0400

    Refactor deleteMember function to streamline member ID retrieval and error handling

--- app/settings/actions.ts
+++ app/settings/actions.ts
@@ -1,12 +1,12 @@
+"use server";
 import { PrismaClient } from "@/generated/prisma";
 import { revalidatePath } from "next/cache";
 
 export async function deleteMember(formData: FormData) {
-  const memberEmail = formData.get("member-email");
-  const householdId = formData.get("household-id");
+  const entryId = formData.get("user-household-id");
 
-  if (!memberEmail || !householdId) {
-    console.error("Member email or household ID is missing");
+  if (!entryId) {
+    console.error("Member ID is missing");
     return;
   }
 
@@ -15,8 +15,7 @@ export async function deleteMember(formData: FormData) {
   // Delete the user household entry
   await prisma.userHousehold.delete({
     where: {
-      // Replace this with the correct unique identifier for the userHousehold table
-      id: formData.get("user-household-id") as string,
+      id: entryId as string,
     },
   });
 
