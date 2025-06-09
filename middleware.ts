fa887fe90c8ac594ef10607113f744a66e01b1b4
TFO\jymele <jymele@tfo.org>
2025-06-09 11:07:30 -0400

    Refactor middleware to improve user authentication checks and enhance code readability

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,34 @@
 // export { auth  } from "@/auth";
 import { auth } from "@/auth";
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "./generated/prisma";
 
 const pathsWithHouseholdIds: string[] = ["/settings", "/addmeal", "/dashboard"];
+const prisma = new PrismaClient();
 
 export async function middleware(request: NextRequest) {
-  // Check if the user is logged in
   const session = await auth();
+
+  /**
+   *   Check if the user is logged in
+   **/
   if (!session) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  // Check if the user is in a room for specific rooms
+  /**
+   *   Check if the user is in a room for specific rooms
+   **/
   const url = new URL(request.url);
   const path = url.pathname;
-  //   console.log("Path is", path);
-  if (!pathsWithHouseholdIds.includes(path)) {
+
+  const userHousehold = await prisma.userHousehold.findFirst({
+    where: {
+      user_email: session.user!.email as string,
+    },
+  });
+
+  if (pathsWithHouseholdIds.includes(path) && userHousehold == null) {
     return NextResponse.redirect(new URL("/new-household", request.url));
   }
 
