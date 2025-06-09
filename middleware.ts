9222b9a9f6f4a92e9d8cfe3174a87d399ae3c523
TFO\jymele <jymele@tfo.org>
2025-06-09 10:49:04 -0400

    Refactor middleware to improve structure and ensure user redirection for household paths

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,23 @@
-export { auth as middleware } from "@/auth";
+export { auth } from "@/auth";
+import { NextRequest } from "next/server";
+import checkIfLoggedIn from "./utils/checkIfLoggedIn";
+import { redirect } from "next/navigation";
+
+const pathsWithHouseholdIds: string[] = ["/settings", "/addmeal", "/dashboard"];
+
+export function middleware(request: NextRequest) {
+  // Check if the user is logged in
+  checkIfLoggedIn();
+
+  // Check if the user is in a room for specific rooms
+  const url = new URL(request.url);
+  const path = url.pathname;
+  //   console.log("Path is", path);
+  if (!pathsWithHouseholdIds.includes(path)) {
+    redirect("new-household");
+  }
+}
+
+export const config = {
+  matcher: ["/settings", "/addmeal", "/dashboard", "/new-household"],
+};
