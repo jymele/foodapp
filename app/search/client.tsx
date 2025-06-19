e0034440cbe4547d2f3dffa0a6e798913649867a
Joel Ymele <jymele@yahoo.fr>
2025-06-19 11:46:31 -0400

    Refactor SearchPage component: update tab triggers for better clarity and remove unused code; adjust default tab to 'week'

--- app/search/client.tsx
+++ app/search/client.tsx
@@ -1,35 +1,24 @@
 "use client";
 import { Tabs, TabsList } from "@/components/ui/tabs";
-// import MealCard from "@/custom/MealCard";
 import MealList from "@/custom/MealList";
 import { Meal } from "@/generated/prisma";
 import { TabsContent, TabsTrigger } from "@radix-ui/react-tabs";
-// import { useState, useEffect } from "react";
-
-// type TimeScale = "day" | "week" | "month";
-
 type SearchPageProps = {
   weekMeals?: Meal[]; // Replace with actual type if available
 };
 
 export default function SearchPage({ weekMeals }: SearchPageProps) {
-  // const [timeScale, setTimeScale] = useState<TimeScale>("day");
-
   return (
     <div>
-      <Tabs defaultValue="day" className="w-full gap-2">
+      <Tabs defaultValue="week" className="w-full gap-4">
         <TabsList className="w-fit mx-auto gap-4">
-          <TabsTrigger value="day" className=" tabtrigger">
-            Day
+          <TabsTrigger value="week" className="tabtrigger">
+            This Week
           </TabsTrigger>
-          <TabsTrigger value="week" className=" tabtrigger">
-            Week
-          </TabsTrigger>
-          <TabsTrigger value="month" className=" tabtrigger">
-            Month
+          <TabsTrigger value="custom-search" className="tabtrigger">
+            Custom
           </TabsTrigger>
         </TabsList>
-        <TabsContent value="day">Days</TabsContent>
         <TabsContent value="week">
           {weekMeals && weekMeals.length > 0 ? (
             <MealList meals={weekMeals} />
@@ -39,7 +28,7 @@ export default function SearchPage({ weekMeals }: SearchPageProps) {
             </p>
           )}
         </TabsContent>
-        <TabsContent value="month">Months</TabsContent>
+        <TabsContent value="custom-search">Months</TabsContent>
       </Tabs>
     </div>
   );
