def2d565c1b970bb366cf563c584817f1b2f200f
TFO\jymele <jymele@tfo.org>
2025-06-24 16:05:53 -0400

    Refactor Profile Page: improve layout and styling for user image and information display

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -6,22 +6,23 @@ export default async function Page() {
 
   return (
     <div className="p-1 min-h-dvh flex flex-col gap-2">
-      <div className="bg-teal-800 primary h-48 w-full rounded-lg p-2">
-        <Image
-          src={session!.user!.image as string}
-          height="500"
-          width="500"
-          className="rounded-full w-24 h-24 ring-2 ring-slate-50"
-          alt={session!.user!.name as string}
-          priority
-        />
-      </div>
-      <div className="flex-1">
-        <div className="text-2xl font-bold">
-          <h2 className="text-2xl font-semibold">{session!.user?.name}</h2>
-          <h3 className="text-base font-medium opacity-70">
-            {session!.user?.email}
-          </h3>
+      <div className="bg-teal-800 primary h-48 w-full rounded-lg p-2"></div>
+      <div className="flex-1 relative p-2">
+        <div className="md:-mt-16 md:ml-4 md:flex md:items-end">
+          <Image
+            src={session!.user!.image as string}
+            height="500"
+            width="500"
+            className="mx-auto -mt-20 md:mt-0 md:mx-0 rounded-full w-36 h-36 ring-3 ring-slate-50"
+            alt={session!.user!.name as string}
+            priority
+          />
+          <div className="p-2 text-center md:text-left md:mb-3">
+            <h2 className="text-2xl font-semibold">{session!.user?.name}</h2>
+            <h3 className="text-base font-medium opacity-70">
+              {session!.user?.email}
+            </h3>
+          </div>
         </div>
       </div>
     </div>
