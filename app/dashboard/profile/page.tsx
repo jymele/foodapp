038badee882e20eed2850c314778bede06eebdcd
TFO\jymele <jymele@tfo.org>
2025-06-24 15:50:15 -0400

    Refactor Profile Page: update layout to display user image and information from session

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,7 +1,29 @@
-export default function Page() {
+import { auth } from "@/auth";
+import Image from "next/image";
+
+export default async function Page() {
+  const session = await auth();
+
   return (
-    <div className="flex items-center justify-center h-dvh">
-      <div className="text-2xl font-bold">Profile Page</div>
+    <div className="p-1 min-h-dvh flex flex-col gap-2">
+      <div className="bg-teal-800 primary h-48 w-full rounded-lg p-2">
+        <Image
+          src={session!.user!.image as string}
+          height="500"
+          width="500"
+          className="rounded-full w-24 h-24 ring-2 ring-slate-50"
+          alt={session!.user!.name as string}
+          priority
+        />
+      </div>
+      <div className="flex-1">
+        <div className="text-2xl font-bold">
+          <h2 className="text-2xl font-semibold">{session!.user?.name}</h2>
+          <h3 className="text-base font-medium opacity-70">
+            {session!.user?.email}
+          </h3>
+        </div>
+      </div>
     </div>
   );
 }
